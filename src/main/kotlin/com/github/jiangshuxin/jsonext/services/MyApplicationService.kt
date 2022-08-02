b27e2e61a469dcccf8edde5e631c6d44422b75c9
GitHub Action <action@github.com>
2022-08-02 02:07:59 +0000

    Template cleanup

--- src/main/kotlin/com/github/jiangshuxin/jsonext/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/jiangshuxin/jsonext/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.jiangshuxin.jsonext.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jiangshuxin.jsonext.MyBundle
 
 class MyApplicationService {
 
